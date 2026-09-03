3d0cde36c7784e7ff00f48e3612e18d76ec751b6
Abdullah Alrasheed <twistedhardware@gmail.com>
2026-09-03 19:52:34 +0800

    sync

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,18 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false
+  },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    server: {
+      hmr: false,
+      ws: false,
+    },
+  },
+  server: {
+    host: true, // required to listen on 0.0.0.0 for external/proxy traffic
+    allowedHosts: ['ln.tejory.io'], // or true to allow all
+  },
 });
