b2a0f2af8ca98f016235a7d98231bdab256e23c4
Abdullah Alrasheed <a@maman.ae>
2026-09-10 00:18:58 +0800

    sync

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import svelte from '@astrojs/svelte';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+	integrations: [
+    svelte(),
+    icon()
+  ],
   devToolbar: {
     enabled: false
   },
